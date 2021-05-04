5890659c1f87a8e09a3b2e9b5e3d68290bad588e
maitham <maithamdib@gmail.com>
2021-05-04 23:45:49 +0100

    Add webhook expired sig test

--- tests/test_vital_python.py
+++ tests/test_vital_python.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pytest
 
 from vital.api.webhooks import Webhooks
+from vital.errors import SignatureVerificationError
 from vital.internal.webhook import WebhookSignature
 
 
@@ -23,3 +24,16 @@ def test_webhook_signature_verification(webhook_secret):
     header = f"t={timestamp},v1={signature},v0=test"
     data = Webhooks.construct_webhook_event(payload, header, webhook_secret)
     assert data.get("test_data") == test_data
+
+
+def test_webhook_signature_verification_rejects_expired_signature(webhook_secret):
+    test_data = "hello"
+    payload = json.dumps({"test_data": test_data})
+    now = datetime.now()
+    timestamp = round(datetime.timestamp(now)) - 500
+    signature = WebhookSignature._compute_signature(
+        f"{timestamp}.{payload}", webhook_secret
+    )
+    header = f"t={timestamp},v1={signature},v0=test"
+    with pytest.raises(SignatureVerificationError):
+        _ = Webhooks.construct_webhook_event(payload, header, webhook_secret)
