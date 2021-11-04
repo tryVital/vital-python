da2ad2514089cb7ff6123d978b530a31875ec57b
maitham <maithamdib@gmail.com>
2021-11-04 11:51:18 +0000

    Add cholesterol method

--- vital/api/vitals.py
+++ vital/api/vitals.py
@@ -28,3 +28,27 @@ class Vitals(API):
                 "provider": provider,
             },
         )
+
+    def cholesterol(
+        self,
+        type: str,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get glucose value API
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data whoop, strava etc.
+        """
+        return self.client.get(
+            f"/vitals/cholesterol/{type}/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
