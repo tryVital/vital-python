49a99a7454216ec66896fba953c748462a82e38e
Paz Saragi <pazsaragi40@gmail.com>
2022-03-25 18:06:16 +0000

    add sleep stream endpoint to sleep client

--- vital/api/sleep.py
+++ vital/api/sleep.py
@@ -25,6 +25,25 @@ class Sleep(API):
             },
         )
 
+    def get_sleep_with_stream(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        GET Sleep data with stream data.
+        """
+        return self.client.get(
+            f"/sleep/{user_key}/stream",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
+
     def get_stream(self, sleep_id: str) -> Mapping[str, List[Mapping]]:
         """
         GET Sleep stream data.
