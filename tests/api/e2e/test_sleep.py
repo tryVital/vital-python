4310c5da263b73a2be05cee1303e752250c4de0d
Paz Saragi <pazsaragi40@gmail.com>
2022-05-04 15:36:38 +0100

    change names

--- tests/api/e2e/test_sleep.py
+++ tests/api/e2e/test_sleep.py
@@ -7,7 +7,7 @@ from vital import Client
 
 @pytest.mark.parametrize(
     "region",
-    ["us", "eu", "us_api_key"],
+    ["us", "eu"],
 )
 def test_sleep_returns_data(
     region,
@@ -51,10 +51,10 @@ def test_sleep_stream_returns_data(
     region,
     user_id: Tuple[str, Client],
 ):
-    start_date = "2021-09-10"
-    end_date = "2021-09-21"
+    start_date = "2022-01-04"
     user_id, client = user_id
-    data = client.Sleep.get_sleep_with_stream(user_id, start_date, end_date)
+    user_id = "0e3ccd2a-3571-44de-99ac-9a5b198a58d0"
+    data = client.Sleep.get_sleep_with_stream(user_id, start_date)
     assert len(data.get("sleep")) > 0
     assert data.get("sleep")[0].get("sleep_stream") is not None
 
@@ -62,3 +62,13 @@ def test_sleep_stream_returns_data(
     data = client.Sleep.get_sleep_with_stream(user_id, start_date)
     assert len(data.get("sleep")) > 0
     assert data.get("sleep")[0].get("sleep_stream") is not None
+
+
+def test_providers(
+    user_id: Tuple[str, Client],
+):
+    user_id, client = user_id
+    start_date = "2021-09-10"
+    end_date = "2021-09-21"
+    data = client.Sleep.get(user_id, start_date, end_date)
+    assert len(data.get("sleep")) > 0
