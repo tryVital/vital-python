50d736439056ebd6b853506de07221de9df423e8
Paz Saragi <pazsaragi40@gmail.com>
2022-05-04 15:38:35 +0100

    undo test changes

--- tests/api/e2e/test_sleep.py
+++ tests/api/e2e/test_sleep.py
@@ -7,7 +7,7 @@ from vital import Client
 
 @pytest.mark.parametrize(
     "region",
-    ["us", "eu"],
+    ["us", "eu", "us_api_key"],
 )
 def test_sleep_returns_data(
     region,
@@ -51,24 +51,14 @@ def test_sleep_stream_returns_data(
     region,
     user_id: Tuple[str, Client],
 ):
-    start_date = "2022-01-04"
+    start_date = "2021-09-10"
+    end_date = "2021-09-21"
     user_id, client = user_id
-    user_id = "0e3ccd2a-3571-44de-99ac-9a5b198a58d0"
-    data = client.Sleep.get_sleep_with_stream(user_id, start_date)
+    data = client.Sleep.get_stream_for_date_range(user_id, start_date, end_date)
     assert len(data.get("sleep")) > 0
     assert data.get("sleep")[0].get("sleep_stream") is not None
 
     # Test no end_date
-    data = client.Sleep.get_sleep_with_stream(user_id, start_date)
+    data = client.Sleep.get_stream_for_date_range(user_id, start_date)
     assert len(data.get("sleep")) > 0
     assert data.get("sleep")[0].get("sleep_stream") is not None
-
-
-def test_providers(
-    user_id: Tuple[str, Client],
-):
-    user_id, client = user_id
-    start_date = "2021-09-10"
-    end_date = "2021-09-21"
-    data = client.Sleep.get(user_id, start_date, end_date)
-    assert len(data.get("sleep")) > 0
