6f6c4a2fa41e8b98a1947232aa9da5cef7c9d1e5
maitham <maithamdib@gmail.com>
2022-11-08 17:46:19 +0000

    Fix endpoint version

--- vital/api/labtests.py
+++ vital/api/labtests.py
@@ -17,7 +17,7 @@ class LabTests(API):
         """Create new order"""
 
         return self.client.post(
-            "/v3/order",
+            "/order",
             {
                 "user_id": user_id,
                 "patient_details": patient_details,
@@ -25,6 +25,7 @@ class LabTests(API):
                 "lab_test_id": lab_test_id,
                 "physician": physician,
             },
+            api_version="v3",
         )
 
     def get_order(self, order_id: str) -> Mapping[str, str]:
@@ -32,36 +33,36 @@ class LabTests(API):
         Get order status.
         :param str user_id: The order_id.
         """
-        return self.client.get(f"/v3/order/{order_id}")
+        return self.client.get(f"/order/{order_id}", api_version="v3")
 
     def cancel_order(self, order_id: str) -> Mapping[str, str]:
         """
         Cancel order.
         :param str user_id: The order_id.
         """
-        return self.client.post(f"/v3/order/{order_id}/cancel")
+        return self.client.post(f"/order/{order_id}/cancel", api_version="v3")
 
     def get_tests(self) -> Mapping[str, str]:
         """
         GET all lab tests the team has access to.
         """
-        return self.client.get("/v3/lab_tests")
+        return self.client.get("/lab_tests")
 
     def get_results(self, order_id: str) -> Mapping[str, str]:
         """
         GET both metadata and raw json test data.
         """
-        return self.client.get(f"/v3/order/{order_id}/result")
+        return self.client.get(f"/order/{order_id}/result", api_version="v3")
 
     def get_results_pdf(self, order_id: str) -> Mapping[str, str]:
         """
         GET gets the lab result for the order in PDF format.
         """
-        return self.client.get(f"/v3/order/{order_id}/result/pdf")
+        return self.client.get(f"/order/{order_id}/result/pdf", api_version="v3")
 
     def get_results_metadata(self, order_id: str) -> Mapping[str, str]:
         """
         GET metadata related to order results, such as
         lab metadata, provider and sample dates.
         """
-        return self.client.get(f"/v3/order/{order_id}/result/metadata")
+        return self.client.get(f"/order/{order_id}/result/metadata", api_version="v3")
