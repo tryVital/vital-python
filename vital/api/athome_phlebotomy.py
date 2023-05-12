67a0a536d2d678c75dc85bf2ee5a0df680c15125
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2023-05-12 15:16:52 +0100

    add typing

--- vital/api/athome_phlebotomy.py
+++ vital/api/athome_phlebotomy.py
@@ -1,7 +1,14 @@
 import typing as t
 import uuid
 
+import pydantic as pyd
+
 from vital.api.api import API
+from vital.api.schema.athome_phlebotomy import (
+    Appointment,
+    AppointmentAvailability,
+    CancellationReason,
+)
 
 
 class AtHomePhlebotomy(API):
@@ -17,7 +24,7 @@ class AtHomePhlebotomy(API):
         state: str,
         zip_code: str,
         unit: t.Optional[str] = None,
-    ) -> t.Mapping[str, t.Any]:
+    ) -> AppointmentAvailability:
         params = {
             "first_line": first_line,
             "second_line": second_line,
@@ -26,58 +33,79 @@ class AtHomePhlebotomy(API):
             "zip_code": zip_code,
             "unit": unit,
         }
-        return self.client.post(
+        response = self.client.post(
             f"/order/{order_id}/phlebotomy/appointment/availability",
             params,
             api_version="v3",
         )
+        response.raise_for_status()
+
+        return AppointmentAvailability.parse_obj(response.json())
 
     def book_appointment(
         self,
         order_id: uuid.UUID,
         booking_key: str,
-    ) -> t.Mapping[str, t.Any]:
-        return self.client.post(
+    ) -> Appointment:
+        response = self.client.post(
             f"/order/{order_id}/phlebotomy/appointment/book",
             {
                 "booking_key": booking_key,
             },
             api_version="v3",
         )
+        response.raise_for_status()
+
+        return Appointment.parse_obj(response.json())
 
     def reschedule_appointment(
         self,
         order_id: uuid.UUID,
         booking_key: str,
-    ) -> t.Mapping[str, t.Any]:
-        return self.client.patch(
+    ) -> Appointment:
+        response = self.client.patch(
             f"/order/{order_id}/phlebotomy/appointment/reschedule",
             {
                 "booking_key": booking_key,
             },
             api_version="v3",
         )
+        response.raise_for_status()
+
+        return Appointment.parse_obj(response.json())
 
     def cancel_appointment(
         self, order_id: uuid.UUID, cancellation_reason_id: uuid.UUID
-    ) -> t.Mapping[str, t.Any]:
+    ) -> Appointment:
         params = {
             "cancellation_reason_id": cancellation_reason_id,
         }
-        return self.client.patch(
+        response = self.client.patch(
             f"/order/{order_id}/phlebotomy/appointment/cancel",
             params,
             api_version="v3",
         )
+        response.raise_for_status()
 
-    def cancellation_reasons(self) -> t.Mapping[str, t.Any]:
-        return self.client.get(
+        return Appointment.parse_obj(response.json())
+
+    def cancellation_reasons(self) -> list[CancellationReason]:
+        response = self.client.get(
             "/order/phlebotomy/appointment/cancellation-reasons",
             api_version="v3",
         )
+        response.raise_for_status()
 
-    def get_appointment(self, order_id: uuid.UUID) -> t.Mapping[str, t.Any]:
-        return self.client.get(
+        return pyd.parse_obj_as(
+            list[CancellationReason],
+            response.json(),
+        )
+
+    def get_appointment(self, order_id: uuid.UUID) -> Appointment:
+        response = self.client.get(
             f"/order/{order_id}/phlebotomy/appointment",
             api_version="v3",
         )
+        response.raise_for_status()
+
+        return Appointment.parse_obj(response.json())
