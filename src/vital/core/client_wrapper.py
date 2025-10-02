130636ba557642069d81876ccef2e1dd61d98008
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-02 13:42:00 +0000

    Release 2.1.416

--- src/vital/core/client_wrapper.py
+++ src/vital/core/client_wrapper.py
@@ -14,10 +14,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "vital/2.1.415",
+            "User-Agent": "vital/2.1.416",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "vital",
-            "X-Fern-SDK-Version": "2.1.415",
+            "X-Fern-SDK-Version": "2.1.416",
         }
         headers["x-vital-api-key"] = self.api_key
         return headers
